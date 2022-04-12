57b544089144031a93d2fb5747bc28d12c5074d3
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-04-12 22:19:40 +0000

    fixed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
 #include "main.h"
-/* print_alphabet- Print all english alphabets
+/**
+ * print_alphabet- Print all english alphabets
  */
 void print_alphabet(void)
 {
 	char k = 'a';
+
 	while (k <= 'z')
 	{
 	_putchar(k);
