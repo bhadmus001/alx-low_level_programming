bd0d7828f85139423eded24a55f96a51defb07aa
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-04-13 14:24:39 +0000

    fix

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - print check for relation
+ * @n: parameter check
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	int result;
+
+	if (n > 0)
+	{
+		result = 1;
+		_putchar('+');
+	}
+	else if (n < 0)
+	{
+		result = -1;
+		_putchar('-');
+	}
+	else
+		result = 0;
+		_putchar('0');
+	return (result);
+}
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - output the last digit of a number
+ * @k: input checker
+ * Return: Always success
+ */
+int print_last_digit(int k)
+{
+	return (k % 10);
+}
+
