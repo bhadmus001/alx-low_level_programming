34414a890da07ad20d8d0a76ab541957d129e449
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-04-13 15:53:10 +0000

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_to_98- prints numbers to 98
+ * @n: character check
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		n--;
+		}
+	}
+	else if (n == 0)
+	{
+		while (n <= 98)
+		{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		n++;
+		}
+	else
+		_putchar(n);
+
+}
