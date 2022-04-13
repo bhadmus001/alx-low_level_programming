cc1484fd3b2c521784787f88ed836d6021592cf5
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-04-13 23:24:31 +0000

    Final update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,11 +6,11 @@ void jack_bauer(void)
 {
 	int h_1, h_2, m_1, m_2;
 	int s = 9;
-	
+
 	h_1 = 0;
 	while (h_1 <= 2)
 	{
-		if ( h_1 == 2)
+		if (h_1 == 2)
 		{
 			s = 3;
 		}
@@ -21,10 +21,10 @@ void jack_bauer(void)
 			while (m_1 <= 5)
 			{
 				m_2 = 0;
-				while (m_2 <=9)
+				while (m_2 <= 9)
 				{
-					_putchar('0' + h_1);
-					_putchar('0' + h_2);
+					_putchar(h_1);
+					_putchar(h_2);
 					_putchar(':');
 					_putchar('0' + m_1);
 					_putchar('0' + m_2);
