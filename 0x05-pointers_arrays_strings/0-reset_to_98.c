590ff27ba3c35c0e4081cd4993c48694e091bc32
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-04-20 22:24:37 +0000

    Array & pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - print 98
+ * @n: parameter checker
  */
 void reset_to_98(int *n)
 {
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - swap the inputs
+ * @a: 1st input
+ * @b: 2nd input
+ */
+void swap_int(int *a, int *b)
+{
+	a = b;
+	b = a;
+}
