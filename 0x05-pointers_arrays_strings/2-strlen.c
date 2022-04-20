5930b1cc5ddc65bb886ed36380a7bc6fc46efca4
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-04-20 23:25:29 +0000

    Array & pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - Return the length of a string
+ * @s: Character input checker
+ * Return: Return the length of string
+ */
+int _strlen(char *s)
+{
+	int i, j, counter;
+
+	i = *s;
+	for (j = 0; j <= i; j++)
+	{
+		counter = 0;
+		counter++;
+	}
+	return (counter);
+}
