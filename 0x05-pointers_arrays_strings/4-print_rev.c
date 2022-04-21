5cff3f411dca1f962fd0704c38251a5e0861cd91
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-04-21 13:53:52 +0000

    Array and pointers tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - Print text in reverse form
+ * @s: Input checker
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+		while (s[i])
+		{
+			_putchar(s[i]);
+			_putchar('\n');
+		}
+	}
+}
