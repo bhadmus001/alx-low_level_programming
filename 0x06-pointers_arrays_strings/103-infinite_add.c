6f118e50936b848a58727dccd513b4dc630c7088
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-04-25 13:38:01 +0000

    New update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -41,6 +41,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		j++;
 	i--;
 	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
 	if (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
@@ -58,7 +60,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			overflow = 0;
 		if (digits >= (size_r - 1))
 			return (0);
-		*(r + digits) = (temp_tot 5 10) + '0';
+		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
 		i--;
