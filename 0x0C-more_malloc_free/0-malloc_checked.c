69fab7fc5351bd0b62578948384afc325f474317
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-09 17:32:58 +0000

    New update on Malloc and free tasks

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - allocate a memory using malloc
+ * @b: size of memory to allocate
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p;
+
+	p = malloc(sizeof(*p) * b);
+
+	if (p == NULL)
+			exit(98);
+}
+		
