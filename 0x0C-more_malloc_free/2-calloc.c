8168710866a3228e8e5e09190d982d53840dd6f4
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-10 21:31:12 +0000

    New update on Malloc and free tasks

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,13 +8,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(size * nmemb);
 		if (p == NULL)
 			return (NULL);
+		for (i = 0; i < nmemb; i++)
+			p[i] = 0;
 		return (p);
 }
 
