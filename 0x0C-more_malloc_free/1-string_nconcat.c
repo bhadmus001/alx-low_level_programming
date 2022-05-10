7fc1ec3c412f924618aabbbf13008d27603ea66d
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-10 17:42:56 +0000

    New update on Malloc and free tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,22 +1,31 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * string_noncat - concatenate 2 strings
- * @s1: pointer to the first string and the destinatuon of the concatenated string
+ * string_nconcat - concatenate 2 strings
+ * @s1: pointer to the first string
  * @s2: pointer to the 2nd string
  * @n: length of string to concatenate
  * Return: pointer to the concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int len, *p;
+	int len, i, j;
+	char *p;
 
-	len = 0;
-	while (s1[len] != '\0')
-	{
-		len++;
-	}
-	p = malloc(sizeof(*p) * len + n);
+	for (len = 0; s1[len]; len++)
+		;
+	p = malloc(sizeof(*p) * (len + n));
 	if (p == NULL)
 		return (NULL);
+	for (i = 0; s1[i]; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		p[i + j] = s2[j];
+	}
+	p[i + j] = '\0';
+	return (p);
+}
 
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: array to allocate memory for
+ * @size: size of memory to allocate
+ * Return: a pointer to the memory
+ */
+void *_calloc(unsigned in nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(size * nmemb);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+}
+
+
