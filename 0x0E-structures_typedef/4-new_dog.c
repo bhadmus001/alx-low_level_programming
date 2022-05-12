905db86cbb716a493721b68b50ec7e629603a703
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-12 00:35:10 +0000

    Programs on structures and typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,16 +10,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_d;
-	unsigned int i, j;
+	unsigned int len, len2, i, j;
 
 	new_d = malloc(sizeof(dog_t));
 	if (new_d == NULL)
 		return (NULL);
+	for (len = 0; name[len]; len++)
+		;
+		new_d->name = malloc(len * sizeof(char));
 	if (new_d->name == NULL)
 		return (NULL);
 	for (i = 0; name[i]; i++)
 		new_d->name[i] = name[i];
 	new_d->age = age;
+	for (len2 = 0; owner[len2]; len2++)
+		;
+		new_d->owner = malloc(len2 * sizeof(char));
 	if (new_d->owner == NULL)
 		return (NULL);
 	for (j = 0; owner[j]; j++)
