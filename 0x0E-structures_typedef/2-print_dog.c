bef84bfae9087567a75cb763b9bf7fd195558ec7
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-12 01:22:08 +0000

    Programs on structures and typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 /**
  * print_dog - print out the deatils of dog in struct dog
  * @d: pointer to the details to be printed
@@ -13,13 +13,13 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: %s\n Age: %.6f\n Owner: %s\n", "(nil)", d->age, d->owner);
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n", "(nil)", d->age, d->owner);
 	}
 	else if (d->owner == NULL)
 	{
-		printf("Name: %s\n Age: %.6f\n Owner: %s\n", d->name, d->age, "(nil)");
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, "(nil)");
 
 	}
 	else
-	printf("Name: %s\n Age: %.6f\n Owner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
