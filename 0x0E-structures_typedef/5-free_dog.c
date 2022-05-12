9e4e8f86852b74d7f33b9f9622cb2c206587d735
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-12 00:01:25 +0000

    Programs on structures and typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,6 +3,7 @@
 /**
  * free_dog - free the allocated memory space
  * @d: pointer to the location to the freed
+ * Description: frees the previously allocated memory
  */
 void free_dog(dog_t *d)
 {
