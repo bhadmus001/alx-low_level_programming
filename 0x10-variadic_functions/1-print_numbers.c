ac1a661b566b72d7083711d3981bed4dbd41e35d
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-16 23:42:50 +0000

    programs on variadic function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,6 +20,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (separator && (i + 1) != n)
 				printf("%s", separator);
 	}
-	printf('\n');
+	printf("\n");
 	va_end(ptr);
 }
