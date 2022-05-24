0f24a3dc75ffaba57b0de5cf6746917eb0bbc55d
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-24 23:23:32 +0000

    Programs on singly linjed lists using C

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * print - prints to the standard output
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
