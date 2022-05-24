c624efc69f03cc77ba9df389b939fb21f66a8dc7
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-24 23:41:08 +0000

    Programs on singly linjed lists using C

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,9 +5,13 @@
  */
 void free_list(list_t *head)
 {
+	list_t *temp;
+
 	while (head)
 	{
-	free(head->str);
-	free(head);
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
 }
