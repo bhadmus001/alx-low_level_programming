f161aabc3572c6849b31b7d0f08583232e2fdca9
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-26 14:19:51 +0000

    More programs on singly linked lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_listint - free list
+ * @head: pointer to the list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t temp;
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
