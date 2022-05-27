f8d4ca2317815ebfb4fb839b8be24046951bfbf6
Badmus Abdulmuhidh <badmusmuhiz@gmail.com>
2022-05-27 01:42:14 +0000

    More programs on singly linked lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,19 @@
  * @head: pointer to the list
  * Return: 0 or the popped out node's data
  */
-int pop_list(listint_t *head)
+int pop_listint(listint_t **head)
 {
-	return ();
+	int result;
+	listint_t *ptr;
+	listint_t *ptr2;
+
+	if (!*head)
+		return (0);
+	ptr2 = *head;
+	result = ptr2->n;
+	ptr = ptr2->next;
+	free(ptr2);
+	*head = ptr;
+	return (result);
+
 }
