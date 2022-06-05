d642107e54651e893430b1cc3ab3263ad3439aac
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-06-05 11:30:17 +0000

    File input and output in C

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,6 +11,8 @@ int create_file(const char *filename, char *text_content)
 	char *buff;
 	int numb_written;
 
+	if (!text_content)
+		text_content = "";
 	count = 0;
 	while (text_content[count])
 	{
@@ -22,8 +24,6 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
-		text_content = "";
 	numb_written = write(fd, text_content, count);
 	if (numb_written == -1)
 		return (-1);
