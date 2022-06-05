ef82496610c75bfbfde42801250b870981842ca3
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-06-05 11:38:20 +0000

    File input and output in C

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,13 +11,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t numb_read, numb_written;
 	char *buffer;
 
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
-		return (0);
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
 		return (0);
 	numb_read = read(fd, buffer, letters);
 	if (numb_read == -1)
