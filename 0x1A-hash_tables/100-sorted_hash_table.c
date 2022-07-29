5527a7b58aa4939cb09b3d827a2d98ecb532fc75
Badmus Abdulmuhidh <Badmusmuhiz@gmail.com>
2022-07-29 22:12:09 +0000

    Hash table using C

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,4 +1,38 @@
-ar *key, const char *value)
+#include "hash_tables.h"
+/**
+ * shash_table_create - creates a shash tables with a given size
+ * @size: size of the shash table
+ * Return: the created shash table, or NuLL if function fails
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *table;
+	shash_node_t **array:
+	unsigned long int i;
+
+	table = malloc(sizeof(shash_table_t));
+	if (table == NULL)
+		return (NULL);
+	array = malloc(sizeof(shash_table_t));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+	table->array = array;
+	table->size = size;
+	table->shead = NULL;
+	table->stall = NULL;
+	return (table);
+}
+
+/**
+ * add_n_shash - adds node at the beginning of a shash at a given index
+ * @h: head of the shash linked list
+ * @key: key of the shash
+ * @value: value to store
+ * Return: created node
+ */
+shash_node_t *add_n_shash(shash_node_t **h, const char *key, const char *value)
 {
 	shash_node_t *tmp;
 
